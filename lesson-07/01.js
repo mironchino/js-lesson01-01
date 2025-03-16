a4e7df206d30aa393e8af5d448325b34d3047232
Miron Ten <mirgoodlife@gmail.com>
2025-03-16 11:01:45 +0900

    lesson7

--- lesson-07/01.js
+++ lesson-07/01.js
@@ -6,4 +6,12 @@
   Другой вариант - использовать метод split строк, и метод массива join.
 */
 
-function capitalizeWords() {}
+function capitalizeWords(string) {
+  let words = string.split(" ");
+  for (let i = 0; i < words.length; i++) {
+    words[i] = words[i].charAt(0).toUpperCase() + words[i].slice(1);
+  }
+  return words.join(" ");
+}
+
+console.log(capitalizeWords("hello world im miron"));
